23c0ade56925350ddc17070fbebc9a8ceb1b49a3
AizeineddineP <AIzeineddine@gmail.com>
2025-01-08 17:59:13 +0300

    Added много всего

--- message.py
+++ message.py
@@ -3,3 +3,27 @@ print(message)
 
 message = "Hello pyhton first workout"
 print(message)
+
+first_name = "Ali"
+family_name = "Zeineddine"
+full_name = first_name + " " +family_name
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+
+message_1 = "Hello, " + full_name.title() + "!"
+print(message_1)
+
+print("Python")
+print("\tPython")
+
+print("Валюты:\nUSD\nEURO\nRUBLES")
+
+print("Валюты:\n\tUSD\n\tEURO\n\tRUBLES")
+
+message_3 = "Валюты:\n\tUSD\n\tEURO\n\tRUBLES"
+
+message_2 = "have "
+print(message_1 + message_2 + message_3)
+
+print("Подарки для жены, список:\n\tМашина\n\tкольцо\n\tотпуск на море ")
